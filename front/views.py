c9a7d6e5d3580fa92d4191441943372a2e726991
asiqurrahman <asiqurrahman13@gmail.com>
2021-02-17 19:41:33 -0500

    Enter

--- front/views.py
+++ front/views.py
@@ -1,40 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
-listings = [
-    {
-        'author' : 'asiqur',
-        'title' : 'Iphone 12',
-        'content' : 'Click the Build tab to bring it forward. From the Work In drop-down list, select the page group that owns the HTML Template. Under the Layout & Appearance section, click the Browse link next to HTML Templates. Click the Copy link next to the HTML Template.',
-        'title2' : 'Samsung 12',
-        'content2' : 'Click the Build tab to bring it forward. From the Work In drop-down list, select the page group that owns the HTML Template. Under the Layout & Appearance section, click the Browse link next to HTML Templates. Click the Copy link next to the HTML Template.',
-        'date_posted' : 'september 1 , 2019'
-    },
-    {
-        'author' : 'asiqur',
-        'title' : 'Iphone 12',
-        'content' : 'Click the Build tab to bring it forward. From the Work In drop-down list, select the page group that owns the HTML Template. Under the Layout & Appearance section, click the Browse link next to HTML Templates. Click the Copy link next to the HTML Template.',
-        'title2' : 'Samsung 12',
-        'content2' : 'Click the Build tab to bring it forward. From the Work In drop-down list, select the page group that owns the HTML Template. Under the Layout & Appearance section, click the Browse link next to HTML Templates. Click the Copy link next to the HTML Template.',
-        'date_posted' : 'september 1 , 2019'
-    },
-    {
-        'author' : 'asiqur',
-        'title' : 'Iphone 12',
-        'content' : 'Click the Build tab to bring it forward. From the Work In drop-down list, select the page group that owns the HTML Template. Under the Layout & Appearance section, click the Browse link next to HTML Templates. Click the Copy link next to the HTML Template.',
-        'title2' : 'Samsung 12',
-        'content2' : 'Click the Build tab to bring it forward. From the Work In drop-down list, select the page group that owns the HTML Template. Under the Layout & Appearance section, click the Browse link next to HTML Templates. Click the Copy link next to the HTML Template.',
-        'date_posted' : 'september 1 , 2019'
-    }
-]
+
 
 def home(request):
     postings = {
-        'listings' : listings 
+        'listings' : Post.objects.all()
     }
     return render(request, 'front/front.html',  postings)
 
 def about(request):
     postings = {
-        'listings' : listings 
+        'listings' : Post.objects.all()
     }
     return render(request, 'front/about.html', postings )
