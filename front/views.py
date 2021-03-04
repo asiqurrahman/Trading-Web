37736adedee3f14b48f40f50ea91279bc71c6b72
asiqurrahman <asiqurrahman13@gmail.com>
2021-03-04 12:55:28 -0500

    create

--- front/views.py
+++ front/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 
@@ -11,15 +13,59 @@ def home(request):
     }
     return render(request, 'front/front.html',  postings)
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'front/front.html'
+    context_object_name = 'listings'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+
+
 def about(request):
     
-    number_of_users = User.objects.all()
+    
     x = User.objects.count()
 
-    
+
     
     number_of_users2 ={
-        'car' : number_of_users,
+        
         'count' : x
     }
 
