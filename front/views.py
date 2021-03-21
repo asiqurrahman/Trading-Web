61cbb59e9b765977ecc9350888c688229630f523
asiqurrahman <asiqurrahman13@gmail.com>
2021-03-21 14:04:35 -0400

    yap

--- front/views.py
+++ front/views.py
@@ -78,11 +78,11 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 def about(request):
     
     x = User.objects.count()
-    loc_key = get('http://ipapi.co/json/?key=dhlDIr1TAg6GdxiKdfn8lVBOmEDOZlXVhPPqfIPKsmujFBXMu6')
+    
 
     #current = request.user.profile.location\
     
-    def get_client_ip(request):
+    def get_ip_address(request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
             ip = x_forwarded_for.split(',')[0]
@@ -90,13 +90,15 @@ def about(request):
             ip = request.META.get('REMOTE_ADDR')
         return ip
     
-    ip = (get_client_ip(request))
+    ip = (get_ip_address(request))
+    #loc_key = get('http://ipapi.co/json/?key=dhlDIr1TAg6GdxiKdfn8lVBOmEDOZlXVhPPqfIPKsmujFBXMu6')
+    #loc = get('https://ipapi.co/{}/country/'.format(ip))
+    #address = loc.json()
+    #city = address['city']
+    #region = address['region']
+    #area = city + ", " + region
     
-    loc = get('https://ipapi.co/json/')
-    address = loc.json()
-    city = address['city']
-    region = address['region']
-    area = city + ", " + region 
+
 
             
 
@@ -104,7 +106,7 @@ def about(request):
     number_of_users2 ={
         
         'count' : x,
-        'location' : area
+        'location' : ip
     
     }
 
