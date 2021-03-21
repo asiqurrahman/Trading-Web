8d3644f3b92a4b63488cc09901d7b702620726a4
asiqurrahman <asiqurrahman13@gmail.com>
2021-03-20 23:29:23 -0400

    whitenoise heroku

--- trade/settings.py
+++ trade/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,5 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'us-east-2' 
+
+django_heroku.settings(locals())
