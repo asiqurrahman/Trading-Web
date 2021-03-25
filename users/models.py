031296b17a89a38234b28e47947566f7aa0f1fc0
asiqurrahman <asiqurrahman13@gmail.com>
2021-03-25 03:49:50 -0400

    new updates

--- users/models.py
+++ users/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 from django.utils import timezone
-import requests 
+import requests
 from requests import get
 from .utils import get_ip_address
 
-class Profile(models.Model):
 
+
+
+class Profile(models.Model):
+    
     area = "placeholder"
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     location = models.CharField(max_length=100, default=area)
 
-   
-    def save(self, request, *args, **kwargs):
-        self.location = get_ip_address(request)
-        super(Profile, self).save(*args, **kwargs)
+
 
     def __str__(self):
         return f'{self.user.username} Profile'
