89718d3660b2330e22abad13331e9ccff8a30e31
asiqurrahman <asiqurrahman13@gmail.com>
2021-04-15 15:09:32 -0400

    navbar

--- front/static/front/app.js
+++ front/static/front/app.js
@@ -1,25 +1,4 @@
-const navSlide = () => {
-    const bruger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    bruger.addEventListener('click', () => {
-        // nav animation
-        nav.classList.toggle('nav-active');
-
-        // nav link animations
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ""
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-    
-        });
-        // close nav animation
-        bruger.classList.toggle('toggle');
-    });
-    
-}
-
-navSlide();
+window.onscroll = () => {
+    const nav = document.querySelector('#navbar');
+    if(this.scrollY <= 700) nav.className = ''; else nav.className = 'scroll';
+  };
